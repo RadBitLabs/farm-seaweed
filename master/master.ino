205fcf8b46ac70e01ee3b599a5f900eaea59dde7
RadBitLabs <30531605+RadBitLabs@users.noreply.github.com>
2017-08-02 23:37:49 +0300

    Add files via upload

--- master/master.ino
+++ master/master.ino
@@ -1,31 +1,25 @@
-
 //Master
 
 #include <SD.h>//Библиотека для SD карты
 #include <SPI.h>
 #include <Wire.h>
+#include <OneWire.h>// Библиотека для DS18B20
 #include <BH1750.h>
-#include <LiquidCrystal_I2C.h>//для аботы с экраном
 #include <DS1302.h> //подключение библиотеки для работы с часами
 #define SensorPin A0            //pH пин к которому подключен pH датчик
 #define Offset -4.0            //Калибровка
 #define samplingInterval 20   //Интервал замеров pH датчика
-#define ArrayLenth  40    //число замеров pH датчика
-
-
-uint16_t Trs;//lux ds18b20
-int CO_2;
-int pH;
-int DO;
-double Ti;// ds18b20 temp in
-double To;// ds8b20 temp out
-int SpM; //digitpot1
-int SpP; //digitpot2
-
+#define ArrayLenth  40    //число замеров pH датчика 
+#define OPROS_SLAVE 2//0-раз в пол минуты,1-раз в минуту,2-раз в две минуты,3-раз в пять минуть
+OneWire  ds_1(22); //Подключение к 22(D) пину
+OneWire  ds_2(23); //Подключение к 23(D) пину
 int pHArray[ArrayLenth];   //Масств в котором будут хранится замеры
 int pHArrayIndex=0;    
   BH1750 lightMeter;
-  int ledPin=13;
+  int tim_sec1;
+  int tim_sec2;
+  int tim_min;
+  
   int EN = 2;//Пин к которому соеденино rs
   int echoPin = 9;//Пин для дальнометра
   int trigPin = 8;//Пин для дальнометра
@@ -36,13 +30,42 @@ int pHArrayIndex=0;
   int  rel_2 = 0;
   int  rel_3 = 0;
   int  rel_4 = 0;
-  int count=1;//Счетчик номера записи в таблицу  
+  long unsigned int count=1;//Счетчик номера записи в таблицу  
+  int count_=0;
+  long int Pulsar;
+  uint16_t lux;
+  int flag_ds_1=0;//Если датчик считал успешно - 0,если нет - 1,2-неправильный адрес
+  int flag_ds_2=0;//Если датчик считал успешно - 0,если нет - 1,2-неправильный адрес
+  float celsius_1;
+  float celsius_2;
   File myFile; // Файл для таблицы
-  LiquidCrystal_I2C lcd(0x3F,20,4);//работа с экраном 20x04
   DS1302 rtc(7, 12, 11); //создание объекта часы RST DAT CLK
   Time t;
 void setup()
 {
+  switch(OPROS_SLAVE)
+  {
+    case 0:
+  tim_sec1=29;
+  tim_sec2=59;
+  tim_min=1;
+  break;
+  case 1:
+  tim_sec1=29;
+  tim_sec2=29;
+  tim_min=1;
+  break;
+  case 2:
+  tim_sec1=29;
+  tim_sec2=29;
+  tim_min=2;
+  break;
+  case 3:
+  tim_sec1=29;
+  tim_sec2=29;
+  tim_min=5;
+  break;
+  }
   rtc.halt(false); //запуск часов
   rtc.writeProtect(false); //снять защиту от записи
   //Установка даты
@@ -50,7 +73,14 @@ void setup()
  // rtc.setDOW(TUESDAY); //установка дня недели (только на английском)                                         2) комментируем эти строчки(что сделано сейчас) и запускаем скетч---------- время сохраняется- часы работают в автономном режиме
  // rtc.setTime(3, 12, 0); //установка времени (часы, минуты, секунды)
  // rtc.setDate(25, 7, 2017); //Дата цифрами. "0" перед одиночной цифрой можно не ставить
- 
+  pinMode(23, OUTPUT );//реле
+  pinMode(24, OUTPUT );//реле
+  pinMode(25, OUTPUT );//реле
+  pinMode(26, OUTPUT );//реле
+  digitalWrite (23, rel_1 ); //реле
+  digitalWrite (24, rel_2 ); //реле
+  digitalWrite (25, rel_3 ); //реле
+  digitalWrite (26, rel_4 ); //реле
   lightMeter.begin();
   if(!SD.begin(PIN_CS)){                           // инициализация SD карты с указанием номера вывода CS
     Serial.println("SD-card not found");   // ошибка инициализации. карта не обнаружена или не подключён (неправильно подключён) адаптер карт MicroSD
@@ -63,7 +93,6 @@ void setup()
   else // иначе если файл есть
     myFile = SD.open("test3.csv", FILE_WRITE);//Открытие или создание файла,где хранится информация
   myFile.close();//Закрытие файла
-  pinMode(ledPin, OUTPUT );
   pinMode(EN, OUTPUT );
   pinMode(trigPin, OUTPUT); //для сонара 
   pinMode(echoPin, INPUT); 
@@ -73,32 +102,44 @@ void setup()
 }
 
 void loop()  
-{  
-   digitalWrite (13, LOW );
-   
-   t = rtc.getTime(); //Инициализация времени
+{
   
-  //Инициализация передачи на Slave-2
-//if(t.sec==3)
- // {
+   
+  t = rtc.getTime(); //Инициализация времени
+  if((t.sec%4==0)&&(t.sec<50))
+  {
+    ReadSensor();
+  }
+
+  if((t.sec==tim_sec1)||(t.sec==tim_sec2)||(t.min==tim_min))
+  {
+     //Инициализация передачи на Slave-2
   digitalWrite (EN, HIGH ); //передача
   delay(10);
   Serial1.print("S2"); //Вызов Slave_2
   delay(10);
   digitalWrite (EN, LOW ); //прием
   delay(10);
+  int S2_count=0;
    Val2 = Serial1.readStringUntil('\n');//считывание
-     if(Val2.indexOf("L") >= 0) 
+      while((Val.indexOf("L") < 0)&&(S2_count < 1)) 
       {
-        Val2.setCharAt(0,' ');
+         digitalWrite (EN, HIGH ); //передача
+         delay(10);
+         Serial1.print("S2");
+         delay(10);
+         digitalWrite (EN, LOW ); //прием
+         delay(20);
+         Val2 = Serial1.readStringUntil('\n');//считывание
+         S2_count++; 
       }
+      if (Val2.indexOf("L") >= 0)
+       Val2.setCharAt(0,' ');
     else
       {
         Serial.println("Slave-2 offline...");
         Val2+=";+;+;+;+;+;+;+;+;+;+;";
       }
-
-
    //Инициализация передачи на Slave-1
    digitalWrite (EN, HIGH ); //передача
    delay(10);
@@ -106,11 +147,21 @@ void loop()
    delay(10);
    digitalWrite (EN, LOW ); //прием
    delay(20);
+   int S1_count=0;
    Val = Serial1.readStringUntil('\n');//считывание
-     if(Val.indexOf("H") >= 0) 
+     while((Val.indexOf("H") < 0)&&(S1_count < 1)) 
       {
-      Val.setCharAt(0,' ');
+         digitalWrite (EN, HIGH ); //передача
+         delay(10);
+         Serial1.print("S1");
+         delay(10);
+         digitalWrite (EN, LOW ); //прием
+         delay(20);
+         Val = Serial1.readStringUntil('\n');//считывание
+         S1_count++; 
       }
+      if (Val.indexOf("H") >= 0)
+       Val.setCharAt(0,' ');
       else
      {
       Serial.println("Slave_1 offline");
@@ -118,10 +169,10 @@ void loop()
      }
       
     WriteFile();  
-//  } 
+  } 
 }
 
-void Writing_Caps()//шапка
+void Writing_Caps()
 {
   if (myFile) 
   {
@@ -196,7 +247,7 @@ double avergearray(int* arr, int number){
   return avg;
 }
 
- int Pulsar()
+ int Pulsar_()
  {
       int duration, mm; 
       digitalWrite(trigPin, LOW); 
@@ -205,12 +256,12 @@ double avergearray(int* arr, int number){
       delayMicroseconds(10); 
       digitalWrite(trigPin, LOW); 
       duration = pulseIn(echoPin, HIGH); 
-     return (duration / 5.8);
+      return (duration / 5.8);
 }
 
   void WriteFile()
   {
-    String test; // Инициализация буфера
+      String test; // Инициализация буфера
       test+=count;//Счетчик строк
       test +=";";
       count++;
@@ -221,19 +272,38 @@ double avergearray(int* arr, int number){
       test +=t.sec;
       test+=';';
       test +=Val;
-      uint16_t lux = lightMeter.readLightLevel();
-      //Trs=lux;// значение для вывода на экран
-      test+=lux;//Датчик мастера BH-1750
+      test+=lux/count_;//Датчик мастера BH-1750
       test+=';';
-      test+='-';//Датчик мастера DS18B20
-      //Ti=     ;//вывод на экран temp in
+      //Датчик мастера DS18B20
+      if (flag_ds_1==1) 
+     {
+        test+="CRC is not valid!";
+     }
+      if (flag_ds_1==2) 
+     {     
+        test+="No more addresses."; 
+     }
+      if (flag_ds_1==0) 
+     {     
+        test+=celsius_1/count_;
+     }
       test+=';';
-      test+='-';//Датчик мастера DS18B20
-      //To=    ;//вывод на экран temp out
+     //Датчик мастера DS18B20
+      if (flag_ds_2==1) 
+     {
+        test+="CRC is not valid!";
+     }
+      if (flag_ds_2==2) 
+     {     
+        test+="No more addresses."; 
+     }
+     if (flag_ds_2==0) 
+     {     
+        test+=celsius_2/count_;
+     }
       test+=';';
-      int PH =PH_Metering();
-      //pH=PH;//вывод на экран 
-      int PH_2=PH/100;
+      int PH =  PH_Metering();
+      int PH_2  = PH/100;
       test+=PH_2;//Датчик мастера PH
       test+=',';
       if (PH%100<10)
@@ -243,9 +313,8 @@ double avergearray(int* arr, int number){
       test+='-';//Датчик мастера 0 ppm
       test+=';';
       test+='-';//Датчик мастера C02 ppm
-      //DO=CO2;// для вывода на экран
       test+=';';    
-      test+=Pulsar();//Датчик мастера растояние
+      test+=Pulsar/count_;//Датчик мастера растояние
       test+=";";
       test+=rel_1;//Мастер SSR_1
       test+=';';
@@ -256,10 +325,8 @@ double avergearray(int* arr, int number){
       test+=rel_4;//Мастер SSR_4
       test+=';'; 
       test+='-';//Мастер digipot_1
-      //SpM=digitpot_1;//вывод на экран
       test+=';';
       test+='-';//Мастер digipot_2
-      //SpP=digitpot_2;//вывод на экран
       test+=';';  
       test+=Val2;
       Serial.println(test);
@@ -267,69 +334,69 @@ double avergearray(int* arr, int number){
       myFile.print(test);
       myFile.println();
       myFile.close();
+      count_=0;
+      Pulsar=0;
+      flag_ds_2=0;
+      celsius_2=0; 
+      flag_ds_1=0;
+      celsius_1=0;
   }
-  /*
-    uint16_t Trs;//lux ds18b20
-    int CO_2;
-    int pH;
-    int DO;
-    double Ti;// ds18b20 temp in
-    double To;// ds8b20 temp out
-    int SpM; //digitpot1
-    int SpP; //digitpot2
-*/
- /*  
-   void DisplayForDevices()
-  {
-      lcd.init();                     
-      lcd.backlight();
-      //=====1 столб данных=====//
-      lcd.setCursor(0,0);
-      lcd.print("Trs-");//100%
-      lcd.setCursor(4,0);
-      lcd.print(Trs);//
-      lcd.setCursor(7,0);
-      lcd.print("%");
-      lcd.setCursor(0,1);
-      lcd.print("CO2-");//200%
-      lcd.setCursor(4,1);
-      lcd.print(CO_2);
-      lcd.setCursor(7,1);
-      lcd.print("%");
-      lcd.setCursor(1,2);
-      lcd.print("pH-");//12.7
-      lcd.setCursor(4,2);
-      lcd.print(pH);
-      lcd.setCursor(1,3);
-      lcd.print("D0-");
-      lcd.setCursor(4,3);
-      lcd.print(DO);
-      lcd.setCursor(6,3);
-      lcd.print("ppm");
-      //=====2 столб данных=====//
-      lcd.setCursor(11,0);
-      lcd.print("Ti-");
-      lcd.setCursor(14,0);
-      lcd.print(Ti);
-      lcd.setCursor(18,0);
-      lcd.print("C");
-      lcd.setCursor(11,1);
-      lcd.print("To-");
-      lcd.setCursor(14,1);
-      lcd.print(To);
-      lcd.setCursor(18,1);
-      lcd.print("C");
-      lcd.setCursor(11,2);
-      lcd.print("SpM-");
-      lcd.setCursor(15,2);
-      lcd.print(SpM);
-      lcd.setCursor(18,2);
-      lcd.print("%");
-      lcd.setCursor(11,3);
-      lcd.print("SpP-");
-      lcd.setCursor(15,3);
-      lcd.print(SpP);
-      lcd.setCursor(18,3);
-      lcd.print("%");
-  }
-  */
+void ReadSensor()
+{
+  count_++;
+  Pulsar+=Pulsar_();
+  lux += lightMeter.readLightLevel();
+  celsius_1+=DS_READ(ds_1,flag_ds_1);
+  celsius_2+=DS_READ(ds_2,flag_ds_2);
+}
+float DS_READ(OneWire ds,bool flag_ds)
+{
+//DS18B20
+      byte i;
+      byte present = 0;
+      byte type_s;
+      byte data[12];
+      byte addr[8];    
+      float celsius;
+      int q=0;
+      while( !ds.search(addr)&&q<2)//Пока адрес неправильный,продолжать проверку адреса!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        {ds.reset_search();
+        q++;
+        }
+      if (OneWire::crc8(addr, 7) != addr[7])
+        flag_ds=1;
+      ds.reset();
+      ds.select(addr);
+      ds.write(0x44, 1);
+  //delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+      present = ds.reset();
+      ds.select(addr);    
+      ds.write(0xBE);         // Read Scratchpad
+      for ( i = 0; i < 9; i++) 
+      {           // we need 9 bytes
+        data[i] = ds.read();
+      }
+      int16_t raw = (data[1] << 8) | data[0];
+      if (type_s) 
+      {
+       raw = raw << 3; // 9 bit resolution default
+       if (data[7] == 0x10) 
+       {// "count remain" gives full 12 bit resolution
+        raw = (raw & 0xFFF0) + 12 - data[6];
+       }
+      }
+      else 
+      {
+        byte cfg = (data[4] & 0x60);
+        // at lower res, the low bits are undefined, so let's zero them
+        if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+        else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+        else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    //// default is 12 bit resolution, 750 ms conversion time
+      }
+      celsius = (float)raw / 16.0;
+      
+}
+
+
